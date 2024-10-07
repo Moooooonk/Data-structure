84f0130fef75c61a91dc9ffbaddc3733e3a6a67b
Moooooonk <j3323968@gmail.com>
2024-10-07 17:14:19 +0900

    vector test1

--- DSP/vector_t.cpp
+++ DSP/vector_t.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 #include<algorithm>
-#include<cstdlib>  
+#include<cstdlib>
 #include<iterator>
 
 using namespace std;
 
+template <class T>
 class vector
 {
 private:
-    int* data;
+    T* data;
     size_t used, space;
-    using iterator = int*;
+    using iterator = T*;
 public:
-    vector():used(0), space(5), data(new int[5]){}
+    vector():used(0), space(5), data(new T[5]){}
     vector(int size);
     ~vector();
 
     vector& operator=(const vector& v);
-    int& operator[](int n);
+    T& operator[](int n);
     void operator()(int n);
 
-    void push_back(int x);
+    void push_back(T x);
     void pop_back();
 
     size_t capacity() const;
@@ -35,19 +36,22 @@ public:
     iterator end();
 };
 
-vector::vector(int size):used(size), space(size), data(new int[size]) {
+template <class T>
+vector<T>::vector(int size):used(size), space(size), data(new T[size]) {
     for(size_t i = 0; i < used; i++) {
-        data[i] = int();
+        data[i] = T();
     }
 }
 
-vector::~vector() { delete[] data; }
+template <class T>
+vector<T>::~vector() { delete[] data; }
 
-vector& vector::operator=(const vector& v) {
+template <class T>
+vector<T>& vector<T>::operator=(const vector& v) {
     if(this == &v) {return *this;}
     if(capacity() != v.capacity()) {
         delete[] data;
-        data = new int[v.capacity()];
+        data = new T[v.capacity()];
         space = v.capacity();
     }
 
@@ -56,62 +60,72 @@ vector& vector::operator=(const vector& v) {
     return *this;
 }
 
-int& vector::operator[](int n) {
-    if(n > size()) {
-        cout<<"vector::operator[]: index"<< n << "out of range\n";
+template <class T>
+T& vector<T>::operator[](int n) {
+    if(n >= size()) {
+        cout<<"vector::operator[]: index "<< n << " out of range\n";
+        exit(1);  // 범위를 벗어난 경우 오류 종료
     }
     return data[n];
 }
 
-void vector::push_back(int x) {
+template <class T>
+void vector<T>::push_back(T x) {
     if(used == capacity()) {
         reserve(size()*2);
     }
     data[used++] = x;
-    used++;
 }
 
-void vector::pop_back() { if(!is_empty) { used--; } }
+template <class T>
+void vector<T>::pop_back() { if(!is_empty()) { used--; } }
 
-size_t vector::capacity() const { return space; }
+template <class T>
+size_t vector<T>::capacity() const { return space; }
 
-size_t vector::size() const { return used; }
+template <class T>
+size_t vector<T>::size() const { return used; }
 
-void vector::reserve(size_t n) {
+template <class T>
+void vector<T>::reserve(size_t n) {
     if(n <= capacity()) { return; }
     if(n < size()) { n = size(); }
     
-    int* temp = new int[n];
+    T* temp = new T[n];
     copy(data, data + used, temp);
     delete[] data;
     data = temp;
     space = n;
 }
 
-bool vector::is_empty() const { 
-    if(size() == 0 ) { return used == 0; } 
-    return false;
+template <class T>
+bool vector<T>::is_empty() const { 
+    return used == 0; 
 }
 
-void vector::clear() { 
+template <class T>
+void vector<T>::clear() { 
     used = 0; 
     space = 5;
     delete[] data;
-    data = new int[space];
+    data = new T[space];
 }
 
-void vector::resize(size_t n) {
+template <class T>
+void vector<T>::resize(size_t n) {
     if(n > used) {
         while(n > used) { 
-            push_back(int()); 
+            push_back(T()); 
         } 
     }
     else if(n < used) { used = n; }
 }
 
-typename vector::iterator vector::begin() { return data; }
+template <class T>
+typename vector<T>::iterator vector<T>::begin() { return data; }
 
-typename vector::iterator vector::end() { return data + used; }
+template <class T>
+typename vector<T>::iterator vector<T>::end() { return data + used; }
 
 int main() {
     vector<int> v(3);
@@ -122,10 +136,12 @@ int main() {
     cout<<"elements of v: ";
     for(int i = 0; i < v.size(); i++) { cout<<v[i]<<" "; }
     cout<<endl<<endl;
+
     for(int i = 0; i < v.size(); i++) { v[i] = i + 1; }
     for(int i = 10; i < 15; i++) {
         v.push_back(i);
     }
+
     cout<<"--- After push_back ---"<<endl;
     cout<<"size of v: "<<v.size()<<endl;
     cout<<"capacity of v: "<<v.capacity()<<endl;
@@ -136,12 +152,12 @@ int main() {
     v.pop_back();
     v.pop_back();
 
-    cout<<"--- After push_back ---"<<endl;
+    cout<<"--- After pop_back ---"<<endl;
     cout<<"size of v: "<<v.size()<<endl;
     cout<<"capacity of v: "<<v.capacity()<<endl;
     cout<<"elements of v: ";
     for(it = v.begin(); it != v.end(); it++) { cout<<*it<<" "; }
     cout<<endl;
-    
+
     return 0;
 }
